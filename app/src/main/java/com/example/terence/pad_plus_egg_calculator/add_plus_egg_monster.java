fdcaacbc706813e4a575ac73ef5ab94f83d023aa
terencehle <terencehle@gmail.com>
2016-07-04 21:44:37 -0700

    new commit with listView errors in setting up two stacked listviews

--- app/src/main/java/com/example/terence/pad_plus_egg_calculator/add_plus_egg_monster.java
+++ app/src/main/java/com/example/terence/pad_plus_egg_calculator/add_plus_egg_monster.java
@@ -1,5 +1,6 @@
 package com.example.terence.pad_plus_egg_calculator;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,11 +9,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 public class add_plus_egg_monster extends AppCompatActivity {
+    private ListView mainListView, bankListView;
+
+    //test monsters
+    private Monster bastet = new Monster("Bastet", 494, 50, 50, 50);
+    private Monster lucifer = new Monster("Archangel Lucifer", 628, 20, 10, 20);
+
+    //array of monsters
+    private Monster[] main_monsters = {bastet};
+    private Monster[] bank_monsters = {lucifer};
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,6 +55,22 @@ public class add_plus_egg_monster extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //declaring the two Listviews
+        mainListView = (ListView) findViewById(R.id.listView1);
+        bankListView = (ListView) findViewById(R.id.listView2);
+
+        //getting adapter to work via http://stackoverflow.com/questions/17693578/android-how-to-display-2-listviews-in-one-activity-one-after-the-other
+        // by Hiren Patel
+        mainListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, main_monsters));
+        bankListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, main_monsters));
+        //current errors.  cannot resolve constructor
+
+        ListUtils.setDynamicHeight(mainListView);
+        ListUtils.setDynamicHeight(bankListView);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -48,5 +93,64 @@ public class add_plus_egg_monster extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("add_plus_egg_monster Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+
+    //copy pasta from the Hiren Patel code
+    public static class ListUtils {
+        public static void setDynamicHeight(ListView mListView) {
+            ListAdapter mListAdapter = mListView.getAdapter();
+            if (mListAdapter == null) {
+                // when adapter is null
+                return;
+            }
+            int height = 0;
+            int desiredWidth = MeasureSpec.makeMeasureSpec(mListView.getWidth(), MeasureSpec.UNSPECIFIED);
+            for (int i = 0; i < mListAdapter.getCount(); i++) {
+                View listItem = mListAdapter.getView(i, null, mListView);
+                listItem.measure(desiredWidth, MeasureSpec.UNSPECIFIED);
+                height += listItem.getMeasuredHeight();
+            }
+            ViewGroup.LayoutParams params = mListView.getLayoutParams();
+            params.height = height + (mListView.getDividerHeight() * (mListAdapter.getCount() - 1));
+            mListView.setLayoutParams(params);
+            mListView.requestLayout();
+        }
     }
 }
